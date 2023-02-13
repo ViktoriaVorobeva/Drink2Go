e0e4b9af08333046ba2cc2da8a178394119cf02f
Viktoria <vorobeva.vik.99@mail.ru>
2023-02-13 17:50:20 +0300

    добавляет часть адаптивных сеток

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const headerNav = document.querySelector('.header-navigation');
+const headerToggle = document.querySelector('.header-navigation__toggle');
+
+headerNav.classList.remove('header-navigation--nojs');
+
+function getNavList () {
+  if(headerNav.classList.contains('header-navigation--opened')) {
+    headerNav.classList.remove('header-navigation--opened');
+    headerNav.classList.add('header-navigation--closed');
+  } else {
+    headerNav.classList.remove('header-navigation--closed');
+    headerNav.classList.add('header-navigation--opened');
+  }
+}
+
+headerToggle.addEventListener('click', getNavList);
